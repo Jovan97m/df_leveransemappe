832c14bc34eff23f1f2cd818bc74d6de8d7aff86
Marezen <48247302+Marezen@users.noreply.github.com>
2021-07-03 10:57:32 +0200

    Search+Sort Fakturaoppsett

--- Telia/TeliaMVC/Controllers/FakturaoppsettsController.cs
+++ Telia/TeliaMVC/Controllers/FakturaoppsettsController.cs
@@ -16,44 +16,124 @@ namespace TeliaMVC.Controllers
         private TeliaEntities db = new TeliaEntities();
 
         // GET: Fakturaoppsetts
-        public ActionResult Index(string sortOrder,int? page)
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
             ViewBag.CurrentSort = sortOrder; 
+            //Viewbags- za sortiranja svake kolone;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.TilegsSortParm = sortOrder == "Tilegs" ? "tilegs_desc" : "Tilegs"; // mislim da ne bi trebalo da ima ova
+            ViewBag.FakturaFormatSortParm = sortOrder == "FakturaFormat" ? "fakturaformat_desc" : "FakturaFormat";
+            ViewBag.FakturaadresseSortParm = sortOrder == "Fakturaadresse" ? "fakturaadresse_desc" : "Fakturaadresse";
+            ViewBag.HusnrSortParm = sortOrder == "Husnr" ? "husnr_desc" : "Husnr";
+            ViewBag.BokstavSortParm = sortOrder == "Bokstav" ? "bokstav_desc" : "Bokstav";
+            ViewBag.PostnummerSortParm = sortOrder == "Postnummer" ? "postnummer_desc" : "Postnummer";
+            ViewBag.StedSortParm = sortOrder == "Sted" ? "sted_desc" : "Sted";
+            //provera za search
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+
+
             var faktures = from s in db.Fakturaoppsetts
                            select s;
+            //pretrazivanje pre rasporedjivanja:
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                faktures = faktures.Where(s => s.NavnPaKostnadssted.Contains(searchString));
+            }
+
+
             switch (sortOrder)
             {
+                //prva kolona
                 case "name_desc":
                     faktures = faktures.OrderByDescending(s => s.NavnPaKostnadssted);
                     break;
+                //druga kolona:
+                case "Tilegs":
+                    faktures = faktures.OrderBy(s => s.Tileggsinfo_kostnadssted);
+                    break;
+                case "tilegs_desc":
+                    faktures = faktures.OrderByDescending(s => s.Tileggsinfo_kostnadssted);
+                    break;
+
+                //treca kolona,fakture
+                case "FakturaFormat":
+                    faktures = faktures.OrderBy(s => s.Fakturaformat);
+                    break;
+                case "fakturaformat_desc":
+                    faktures = faktures.OrderByDescending(s => s.Fakturaformat);
+                    break;
+
+                //cetvrta kolona,adrese za fakture
+                case "Fakturaadresse":
+                    faktures = faktures.OrderBy(s => s.Fakturaadresse);
+                    break;
+                case "fakturaadresse_desc":
+                    faktures = faktures.OrderByDescending(s => s.Fakturaadresse);
+                    break;
+
+                //Peta kolona, Husnr
+                case "Husnr":
+                    faktures = faktures.OrderBy(s => s.Husnr);
+                    break;
+                case "husnr_desc":
+                    faktures = faktures.OrderByDescending(s => s.Husnr);
+                    break;
+
+                //sesta kolona, Bokstav , mozda i ne mora
+                case "Bokstav":
+                    faktures = faktures.OrderBy(s => s.Bokstav);
+                    break;
+                case "bokstav_desc":
+                    faktures = faktures.OrderByDescending(s => s.Bokstav);
+                    break;
+
+                //sedma kolona PostNummer
+                case "Postnummer":
+                    faktures = faktures.OrderBy(s => s.Postnummer);
+                    break;
+                case "postnummer_desc":
+                    faktures = faktures.OrderByDescending(s => s.Postnummer);
+                    break;
+
+                //osma kolona Sted
+                case "Sted":
+                    faktures = faktures.OrderBy(s => s.Sted);
+                    break;
+                case "sted_desc":
+                    faktures = faktures.OrderByDescending(s => s.Sted);
+                    break;
+
                 default:  // Name ascending 
                     faktures = faktures.OrderBy(s => s.NavnPaKostnadssted);
                     break;
             }
 
 
-            int pageSize = 5;
+            int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(faktures.ToPagedList(pageNumber, pageSize));
         }
-
-        // GET: Fakturaoppsetts/Details/5
-        public ActionResult Details(string id)
+       
+        protected override void Dispose(bool disposing)
         {
-            if (id == null)
+            if (disposing)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                db.Dispose();
             }
-            Fakturaoppsett fakturaoppsett = db.Fakturaoppsetts.Find(id);
-            if (fakturaoppsett == null)
-            {
-                return HttpNotFound();
-            }
-            return View(fakturaoppsett);
+            base.Dispose(disposing);
         }
 
-        // GET: Fakturaoppsetts/Create
+        //
+        #region CRUD operacije
         public ActionResult Create()
         {
             return View();
@@ -132,14 +212,19 @@ namespace TeliaMVC.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-
-        protected override void Dispose(bool disposing)
+        public ActionResult Details(string id)
         {
-            if (disposing)
+            if (id == null)
             {
-                db.Dispose();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            base.Dispose(disposing);
+            Fakturaoppsett fakturaoppsett = db.Fakturaoppsetts.Find(id);
+            if (fakturaoppsett == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fakturaoppsett);
         }
+        #endregion
     }
 }
