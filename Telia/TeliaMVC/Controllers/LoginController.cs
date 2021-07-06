20b292f353fa5cfd8a944469b791a6fe33eb06b7
Jovan97m <62722395+Jovan97m@users.noreply.github.com>
2021-07-06 02:26:54 +0200

    Login

---
+++ Telia/TeliaMVC/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeliaMVC.Models;
+
+namespace TeliaMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorise(Admin admin)
+        {
+            using (TeliaEntities db = new TeliaEntities())
+            {
+                var userDetail = db.Admins.Where(x => x.UserName == admin.UserName && x.Password == admin.Password).FirstOrDefault();
+                if (userDetail == null)
+                {
+                    admin.LoginErrorMsg = "Invalid UserName or Password";
+                    return View("Index", admin);
+                }
+                else
+                {
+                    Session["Id"] = admin.Id;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
--- Telia/TeliaMVC/Models/Admin.cs
+++ Telia/TeliaMVC/Models/Admin.cs
@@ -7,15 +7,25 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web.Mvc;
+
 namespace TeliaMVC.Models
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Admin
     {
         public int Id { get; set; }
+        [Display(Name = "UserName")]
+        [Required(ErrorMessage = "This field is Required")]
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is Required")]
         public string Password { get; set; }
+
+        public string LoginErrorMsg { get; set; }
     }
 }
