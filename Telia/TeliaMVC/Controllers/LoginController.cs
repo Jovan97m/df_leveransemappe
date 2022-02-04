1acbe0c2c0217da6a554439ed4656c7d7a6fde1e
Marezen <48247302+Marezen@users.noreply.github.com>
2022-02-04 22:42:14 +0100

    Dodata nova stranica za Login. Posebna za client- posebno za admin

--- Telia/TeliaMVC/Controllers/LoginController.cs
+++ Telia/TeliaMVC/Controllers/LoginController.cs
@@ -19,33 +19,15 @@ namespace TeliaMVC.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Authorise(Admin admin, Client client,String usertype)
+        public ActionResult Authorise(Admin admin)
 
         {
             using (TeliaEntities db = new TeliaEntities())
             {
-                if (usertype == "admin")
-                {
-                    var userDetail = db.Admins.Where(x => x.UserName == admin.UserName && x.Password == admin.Password).FirstOrDefault();
-                    if (userDetail == null)
-                    {
-                        admin.LoginErrorMsg = "Invalid UserName or Password";
-                        admin.UserName = "";
-                        admin.Password = "";
-                        return View("Index", admin);
-                    }
-                    else
-                    {
-                        Session["Id"] = admin.Id;
-                        return RedirectToAction("Index", "Home");
-                    }
-                }
-                else if (usertype=="client")
-                {
                     var userDetail = db.Clients.Where(x => x.Orgnummer == admin.UserName  && x.Password == admin.Password).FirstOrDefault();
                     if (userDetail == null)
                     {
-                        admin.LoginErrorMsg = "Invalid UserName or Password";
+                        admin.LoginErrorMsg = "Invalid Orgnummer or Password";
                         admin.UserName = "";
                         admin.Password = "";
                         return View("Index", admin);
@@ -55,8 +37,6 @@ namespace TeliaMVC.Controllers
                         Session["Id"] = userDetail.Id; // mora orgnummer,da bi mogao da ima njegov broj firme
                         return RedirectToAction("Details", "HomeClient",new { userDetail.Id});
                     }
-                }
-                else { return View(); }
             }
 
         }
@@ -64,9 +44,35 @@ namespace TeliaMVC.Controllers
         public ActionResult LogOut()
         {
             Session.Abandon();
-            ViewBag.Admin = "admin";
             ViewBag.Client = "client";
             return RedirectToAction("Index", "Login");
+        }
+
+        public ActionResult LoginAdmin()
+        {
+            ViewBag.Admin = "admin";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AuthoriseAdmin(Admin admin)
+        {
+            using (TeliaEntities db = new TeliaEntities())
+            {
+                var userDetail = db.Admins.Where(x => x.UserName == admin.UserName && x.Password == admin.Password).FirstOrDefault();
+                if (userDetail == null)
+                {
+                    admin.LoginErrorMsg = "Invalid UserName or Password";
+                    admin.UserName = "";
+                    admin.Password = "";
+                    return View("LoginAdmin", admin);
+                }
+                else
+                {
+                    Session["Id"] = admin.Id;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
         }
     }
 }
